52cf10b538c826de507a4fbe36a186cb2d01ad5e
Giovanna Juarez <giobjuarez@gmail.com>
2015-10-21 11:08:19 -0400

    done with class

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,12 +1,50 @@
 students = [
   "Giovanna",
   "Naty",
-  "Josh",
+  "Alberto",
   "Carlos",
   "Beatriz",
-  "Richard",
-  "Gera",
+  "Carolina",
+  "Gabriela",
   "Alex",
   "Jacky"
 ]
-puts "#{students}"
+
+students = []
+complete = false
+
+puts "Who came to class today? (type done when finish)"
+until complete == true
+  name = gets.chomp.downcase.capitalize
+  if name == "Done"
+    complete = true
+  else
+    students.push(name)
+  end
+end
+
+students.shuffle!
+
+count = 0
+num_students = students.length
+
+until count == num_students
+  if num_students == 1
+    puts "#{students[count]} is flying solo today."
+    count += 1
+  else
+
+      if num_students % 2 == 1
+
+        if count < num_students - 3
+          puts "#{students[count]} & #{students[count+1]}"
+        else
+          puts "#{students[count]}, #{students[count+1]} & #{students[count+2]}"
+          break
+        end
+      else
+        puts "#{students[count]} & #{students[count+1]}"
+      end
+    count += 2
+    end
+end
